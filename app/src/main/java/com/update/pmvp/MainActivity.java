e8443d95052cb8e83c79ed3be0647c653c49594d
liupu <575866447@qq.com>
2019-04-19 21:46:04 +0800

    抽取base库

--- app/src/main/java/com/update/pmvp/MainActivity.java
+++ app/src/main/java/com/update/pmvp/MainActivity.java
@@ -3,6 +3,11 @@ package com.update.pmvp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * author : liupu
+ * date   : 2019/4/19
+ * desc   :
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
---
+++ module_base/src/main/java/com/update/module_base/utils/L.java
@@ -0,0 +1,9 @@
+package com.update.module_base.utils;
+
+/**
+ * author : liupu
+ * date   : 2019/4/19
+ * desc   :
+ */
+public class L {
+}
