62ed31f5d5b3179a52b36abba05cb1e0824c632d
liupu <575866447@qq.com>
2019-08-23 09:52:01 +0800

    网络base返回值优化

--- module_base/src/main/java/com/update/module_base/net/NetEntity.java
+++ module_base/src/main/java/com/update/module_base/net/NetEntity.java
@@ -1,13 +1,17 @@
 package com.update.module_base.net;
 
 
+import java.io.Serializable;
+
 /**
  * @author : liupu
  * date   : 2019/8/22
  * desc   : 网络返回数据的javaBean(泛型解析)
  */
-public class NetEntity<T> extends BaseResult {
+public class NetEntity<T> implements Serializable {
 
+    public String code;
+    public String msg;
     public T data;
 
 }
