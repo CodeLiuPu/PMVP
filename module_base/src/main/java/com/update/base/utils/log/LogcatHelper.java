b1c91c2ae1b175a64c49f877a5ee81c86e931301
liupu <575866447@qq.com>
2019-08-31 23:40:43 +0800

    LogcatHelper add

---
+++ module_base/src/main/java/com/update/base/utils/log/LogcatHelper.java
@@ -0,0 +1,11 @@
+package com.update.base.utils.log;
+
+/**
+ * @author : liupu
+ * date    : 2019/8/31
+ * desc    :
+ * github : https://github.com/CodeLiuPu/
+ */
+public class LogcatHelper {
+
+}
