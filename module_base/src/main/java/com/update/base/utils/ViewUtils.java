bca6eb2f4645ac5ae41f031e6350f1ab543ad74e
liupu <575866447@qq.com>
2019-09-07 19:18:10 +0800

    ViewUtils 完善

--- module_base/src/main/java/com/update/base/utils/ViewUtils.java
+++ module_base/src/main/java/com/update/base/utils/ViewUtils.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.support.annotation.IdRes;
 import android.view.View;
+import android.view.ViewStub;
 import android.view.Window;
 
 /**
@@ -35,5 +36,44 @@ public final class ViewUtils {
     @SuppressWarnings("unchecked")
     public static <V extends View> V findViewById(Window window, @IdRes int id) {
         return (V) window.findViewById(id);
+    }
+
+    public static boolean isVisible(View _view) {
+        return _view.getVisibility() == View.VISIBLE;
+    }
+
+    public static boolean isGone(View view) {
+        return view.getVisibility() == View.GONE;
+    }
+
+    public static void setGone(View... _view) {
+        setVisibility(View.GONE, _view);
+    }
+
+    public static void setInvisible(View... _view) {
+        setVisibility(View.INVISIBLE, _view);
+    }
+
+    public static void setVisible(View... _view) {
+        setVisibility(View.VISIBLE, _view);
+    }
+
+    public static void setVisibility(boolean isVisible, View... views) {
+        if (isVisible) {
+            setVisible(views);
+        } else {
+            setGone(views);
+        }
+    }
+
+    public static void setVisibility(int visibility, View... _view) {
+        for (View view : _view) {
+            if (view != null) {
+                if (view instanceof ViewStub || view.getVisibility() != visibility) {
+                    view.setVisibility(visibility);
+                }
+
+            }
+        }
     }
 }
